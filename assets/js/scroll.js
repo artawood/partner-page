86d5eb3ed819e3bd4f7a59ef5b814c4605cbf1d5
Artawood Chitamitara <artawood@gmail.com>
2019-05-30 03:40:22 -0700

    fixes per QA

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
-  $("a[href*=#]").bind("click", function(e) {
+jQuery(document).ready(function() {
+  jQuery("a[href*=\\#]").bind("click", function(e) {
     e.preventDefault(); // prevent hard jump, the default behavior
 
-    var target = $(this).attr("href"); // Set the target as variable
+    var target = jQuery(this).attr("href"); // Set the target as variable
 
     // perform animated scrolling by getting top-position of target-element and set it as scroll target
-    $("html, body")
+    jQuery("html, body")
       .stop()
       .animate(
         {
-          scrollTop: $(target).offset().top
+          scrollTop: jQuery(target).offset().top
         },
         600,
         function() {
